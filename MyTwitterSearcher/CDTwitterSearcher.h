106829501005dc4040051179ce6ca1b44c2d3330
Mark Wilkinson <markawil@yahoo.com>
2012-04-09 19:10:40 -0500

    Initial setup with asi-http, json and rechability libraries

---
+++ MyTwitterSearcher/CDTwitterSearcher.h
@@ -0,0 +1,13 @@
+//
+//  CDTwitterSearcher.h
+//  MyTwitterSearcher
+//
+//  Created by Mark Wilkinson on 4/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CDTwitterSearcher : NSObject
+
+@end
---
+++ MyTwitterSearcher/CDTwitterSearcher.m
@@ -0,0 +1,13 @@
+//
+//  CDTwitterSearcher.m
+//  MyTwitterSearcher
+//
+//  Created by Mark Wilkinson on 4/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CDTwitterSearcher.h"
+
+@implementation CDTwitterSearcher
+
+@end
